f5d1ec99384dab56586569c98123403280c125e5
vivektiwary449-ux <vivektiwary449@gmail.com>
2026-02-01 19:50:56 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+# writing input functions
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+# writing the calculation function
+add = int(num1) + int(num2)
+print(" addition : " + str(add))
+sub = int(num1) - int(num2)
+print(" subtraction : " + str(sub))
+mul = int(num1) * int(num2)
+print(" multiplication : " + str(mul))
+div = int(num1) / int(num2)
+print(" division : " + str(div))
---
+++ task2.py
@@ -0,0 +1,5 @@
+# taking input from the user
+first_name = input("enter your first name: ")
+last_name  = input("enter your last name: ")
+add = str(first_name) + str(last_name)
+print("hello, " + add +"! welcome to the python program")
